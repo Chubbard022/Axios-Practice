b3e21dda7968ca2c1978673c5fdee66867746479
Chubbard022 <chubbard022@gmail.com>
2019-05-17 14:55:57 -0700

    server working, starting on other tables

--- api/server.js
+++ api/server.js
@@ -1,6 +1,5 @@
 const express = require("express")
 const helmet = require("helmet")
-const projectRouter = require("../projects/projectRoutes")
 const actionRouter = require("../projects/actionRoutes")
 
 const server = express()
@@ -8,8 +7,7 @@ const server = express()
 server.use(express.json())
 server.use(helmet())
 
-server.use("/api/projects",projectRouter)
-server.use("/api/actions",actionRouter)
+server.use("/api/action",actionRouter)
 
 server.get("/",(req,res)=>{
     res.status(200).json("server working")
--- projects/projectRoutes.js
+++
@@ -1,7 +0,0 @@
-const knex = require("knex")
-const router = require("express").Router()
-const knexConfig = require("../knexfile")
-const db = knex(knexConfig.development);
-
-
-module.exports = router;
